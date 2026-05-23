7e8257a01b94eff5a1ab17ce98b7d01c50e67fbb
kimssi1201 <kimssi1201@gmail.com>
2026-05-23 13:38:15 +0000

    Upload current project code

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,45 +1,38 @@
-# To learn more about how to use Nix to configure your environment
-# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
-  # Which nixpkgs channel to use.
-  channel = "stable-24.11"; # or "unstable"
-  # Use https://search.nixos.org/packages to find packages
-  packages = [
-    pkgs.nodejs_22
-    pkgs.python3
-  ];
-  # Sets environment variables in the workspace
-  env = {};
-  idx = {
-    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
-    extensions = [
-      # "vscodevim.vim"
-      "google.gemini-cli-vscode-ide-companion"
+    # Which nixpkgs channel to use.
+    channel = "stable-23.11"; # or "unstable"
+    # Use https://search.nixos.org/packages to find packages
+    packages = [
+      pkgs.nodejs_20
+      pkgs.python3
+      pkgs.pip
+      pkgs.python3Packages.openai
     ];
+    # Sets environment variables in the workspace
+    env = {};
+    # Fast way to run commands without creating a separate script.
+    # Also see .idx/previews.json for preview definitions.
+    onCreate = {
+      # ExampleDevServer = "npm run dev";
+    };
+    # The image of the workspace. Use this to run custom commands when the
+    # workspace is created.
+    onCreate.init = '''
+      # Example
+      # npm install
+    ''';
     # Enable previews and customize configuration
     previews = {
       enable = true;
       previews = {
         web = {
-          command = ["python3" "-m" "http.server" "$PORT" "--bind" "0.0.0.0"];
+          command = [
+            "npm"
+            "run"
+            "dev"
+          ];
           manager = "web";
         };
       };
     };
-    # Workspace lifecycle hooks
-    workspace = {
-      # Runs when a workspace is first created
-      onCreate = {
-        # Example: install JS dependencies from NPM
-        # npm-install = "npm install";
-        # Open editors for the following files by default, if they exist:
-        default.openFiles = [ "style.css" "main.js" "index.html" ];
-      };
-      # Runs when the workspace is (re)started
-      onStart = {
-        # Example: start a background task to watch and re-build backend code
-        # watch-backend = "npm run watch-backend";
-      };
-    };
-  };
-}
+  }
